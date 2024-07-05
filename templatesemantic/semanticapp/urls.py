7a6efdd1a7cf74a9e4594251a998b2544783dd12
Arnold Saldivar <saldivararnold80@gmail.com>
2024-07-05 12:15:40 +0800

    added login logout

--- templatesemantic/semanticapp/urls.py
+++ templatesemantic/semanticapp/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
     path("scan/", views.scan, name="scan"),
     path("attendance/", views.attendance, name="attendance"),
+    path("login/", views.user_login, name="login"),
+    path("logout/", views.user_logout, name="logout"),
 ]
--- templatesemantic/semanticapp/views.py
+++ templatesemantic/semanticapp/views.py
@@ -1,13 +1,38 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import *
+from django.contrib import messages
 
 
 # Create your views here.
-def login(request):
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+    if request.method == "POST":
+        userform = LoginForm(request.POST)
+        if userform.is_valid():
+            username = userform.cleaned_data["username"]
+            password = userform.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+            else:
+                messages.warning(request, "Wrong username or password")
+    loginform = LoginForm()
+
     title = "Login"
     context = {
         "title": title,
+        "loginform": loginform,
     }
     return render(request, "login.html", context)
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("login")
 
 
 def home(request):
